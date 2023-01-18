04ba2d81d8d9f98199f9f53b1e55136f01f83157
Matthew <104280624+mattkaay@users.noreply.github.com>
2023-01-18 15:59:25 +1300

    (Open discord ticket for FOUR airdrops).

--- nft-minting-app-main/src/App.js
+++ nft-minting-app-main/src/App.js
@@ -145,7 +145,7 @@ function App() {
       .then((receipt) => {
         console.log(receipt);
         setFeedback(
-          `WOW, the ${CONFIG.NFT_NAME} is yours! go visit Opensea.io to view it.`
+          `WOW, the ${CONFIG.NFT_NAME} is yours! (Open discord ticket for FOUR airdrops).`
         );
         setClaimingNft(false);
         dispatch(fetchData(blockchain.account));
